a8afef9fcf2a9623a3752cd3ef3cf2a44525f056
gabriela zapata <zapatagabrielagabriela203@gmail.com>
2025-07-02 10:03:14 -0500

    recetas deliciosas

---
+++ RECETAS/Panqueques.py
@@ -0,0 +1,13 @@
+🥞 Panqueques Caseros
+
+**Ingredientes:**
+- 1 taza de harina
+- 1 huevo
+- 1 taza de leche
+- 1 cda de azúcar
+- 1 cdita de polvo de hornear
+
+**Preparación:**
+1. Mezclar todos los ingredientes.
+2. Verter en una sartén caliente y dorar por ambos lados.
+3. Servir con miel o frutas
